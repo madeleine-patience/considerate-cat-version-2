8dae136f006db39b4b5eda65e3b8ea3fb5dced9c
madeleinepbelanger <madeleinepbelanger@gmail.com>
2024-01-05 19:00:30 -0800

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { ThemeProvider } from "@mui/material";
 import { defaultTheme } from "./theme/themeBuilder";
 import Home from "./pages/Home";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Home />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={defaultTheme}>
+        <Home />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
