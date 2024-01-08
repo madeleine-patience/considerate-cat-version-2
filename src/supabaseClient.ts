0c36deeed6689894863507ba55aa13c1894e1b2f
madeleinepbelanger <madeleinepbelanger@gmail.com>
2024-01-08 14:37:28 -0800

    Updated ENV file

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://bmxnsuildxczrsqnmyje.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJteG5zdWlsZHhjenJzcW5teWplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDMxMTYyMzIsImV4cCI6MjAxODY5MjIzMn0.eKrZxTfD1sNZV0GmPuLQ_sPYTPecCQJo_DNbwIbyXSE";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
